ec6614b777a92b75b4b7a9ca93f6c1ebbdedcb3e
Charles Chavis <chavisca@gmail.com>
2023-10-24 03:43:06 -0400

    added logic to database.JS and plugin configuration for webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const addTextDb = await openDB('jate', 1);
+  const jateTX = addTextDb.transaction('jate', 'readwrite');
+  const store = jateTX.objectStore('jate');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async (id, content) => {
+  console.log('GET all from the database');
+  const allTextDb = await openDB('jate', 1);
+  const jateTX = allTextDb.transaction('jate', 'readonly');
+  const store = jateTX.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 initdb();
